cd4f6f3b8f03538a9f6bf30f1bea84f79b7d7675
arun <arun.chan86@gmail.com>
2023-08-24 13:16:01 +0530

    11- Create-Register and Login -Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import Home from "./pages/Home";
 import ProductsList from "./pages/ProductsList";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 
 const App = () => {
   return(
   <div>
     {/* <Home /> */}
     {/* <ProductsList /> */}
-    <Product />
+    {/* <Product /> */}
+    {/* <Register /> */}
+    <Login />
   </div>
   ) 
 };
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://img.freepik.com/free-photo/medium-shot-woman-holding-shopping-bags_23-2148610280.jpg?w=1060&t=st=1692862678~exp=1692863278~hmac=693234dc2d40262bfe9b55e9b30497065d9372b83e7f7a4e7f530d0d9b8d4931")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+ 
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Login
