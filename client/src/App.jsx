b7f5cdefa6070c372ff94ce7dbabb4c9fb02b9ab
arun <arun.chan86@gmail.com>
2023-08-24 11:26:11 +0530

    10-Add-SingleProduct-Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 import ProductsList from "./pages/ProductsList";
+import Product from "./pages/Product";
 
 const App = () => {
   return(
   <div>
     {/* <Home /> */}
-    <ProductsList />
+    {/* <ProductsList /> */}
+    <Product />
   </div>
   ) 
 };
